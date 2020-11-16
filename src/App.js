f3123bf5d2c34377c5b4e96f97b71d7476436222
mfpardini <matheusfpardini@gmail.com>
2020-11-15 21:03:46 -0300

    added Bootstrap CSS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+import React, { Component } from 'react';
+import NavigationBar from './components/NavigationBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavigationBar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          testing app
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
